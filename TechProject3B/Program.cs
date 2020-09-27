ff9f6723afce4fcd8e1500e82a479af66c52c1b0
Nikhil Patel <nikhil8@usf.edu>
2020-09-27 15:51:59 -0700

    Added a date

--- TechProject3B/Program.cs
+++ TechProject3B/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿// Programmer: Nikhil Patel
+// Date: 09/27/2020
+// Description: This code demonstrates a manual array 
+
+using System;
 
 namespace TechProject3B
 {
@@ -6,7 +10,25 @@ namespace TechProject3B
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+                int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25 };
+
+                // for-each loop used
+                foreach (int n in numbers)
+                {
+                    Console.Write(" Elemental Value = " + n);
+                    Console.WriteLine("");
+
+
+                }
+
+            Console.WriteLine("");
+            // Exit access for user
+            Console.WriteLine(" Press any key to exit the program ");
+            // Used to keep the program running
+            Console.ReadKey(true);
+
+            
         }
     }
 }
